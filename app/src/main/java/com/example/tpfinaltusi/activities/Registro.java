da87118c6fdbc53482549992f2e9b3d92579cf75
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-09-24 15:44:20 -0300

    Falta terminar

--- app/src/main/java/com/example/tpfinaltusi/activities/Registro.java
+++ app/src/main/java/com/example/tpfinaltusi/activities/Registro.java
@@ -3,31 +3,24 @@ package com.example.tpfinaltusi.activities;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.tpfinaltusi.R;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class Login extends AppCompatActivity {
-    private EditText etEmail;
-    private EditText etPassword;
-    private Button btnLogin;
-    private ImageButton togglePasswordButton;
-    private TextView btnOlvidasteContraseña;
-    private TextView btnRegistrarse;
+public class Registro extends AppCompatActivity {
     boolean passwordVisible = true;
+    boolean repeatPasswordVisible = true;
+    private EditText etPassword;
+    private ImageButton togglePasswordButton;
+    private EditText etRepeatPassword;
+    private ImageButton toggleRepeatPasswordButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,24 +38,18 @@ public class Login extends AppCompatActivity {
 
         // Configurar el título centrado (opcional)
         TextView actionBarTitle = customActionBarView.findViewById(R.id.action_bar_title);
-        actionBarTitle.setText("Login");
+        actionBarTitle.setText("Registro");
 
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registro);
         /////////////////////////////////////OBTENER ELEMENTOS///////////////////////////////////////////////
-        btnLogin = findViewById(R.id.btn_login);
-        etEmail = findViewById(R.id.et_mail);
         etPassword = findViewById(R.id.et_password);
         togglePasswordButton = findViewById(R.id.togglePasswordButton);
-        btnRegistrarse = findViewById(R.id.btn_registrarse);
-        btnOlvidasteContraseña = findViewById(R.id.btn_olvidasteContraseña);
-
+        etRepeatPassword = findViewById(R.id.et_repeatPassword);
+        toggleRepeatPasswordButton = findViewById(R.id.toggleRepeatPasswordButton);
         /////////////////////////////////////FUNCIONES COMPORTAMIENTO////////////////////////////////////////
         comportamientoMostrarOcultarContrasenia();
-        comportamientoBotonRegistrarse();
-        comportamientoBotonOlvidasteContrasenia();
-        comportamientoBotonLogin();
+        comportamientoMostrarOcultarContraseniaRepetida();
     }
-
     private void comportamientoMostrarOcultarContrasenia(){
         //Controlar mostrar ocultar contraseña
         togglePasswordButton.setBackgroundResource(R.drawable.eye_svgrepo_com); // Icono de ojo abierto
@@ -82,40 +69,22 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-    private void comportamientoBotonRegistrarse(){
-        //Control boton registrarse
-        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+    private void comportamientoMostrarOcultarContraseniaRepetida(){
+        //Controlar mostrar ocultar contraseña
+        toggleRepeatPasswordButton.setBackgroundResource(R.drawable.eye_svgrepo_com); // Icono de ojo abierto
+        toggleRepeatPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Registro.class);
-                startActivity(intent);
-            }
-        });
-    }
-    private void comportamientoBotonOlvidasteContrasenia(){
-        //Control boton olvidaste contraseña
-        btnOlvidasteContraseña.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "FALTA HACER XD", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-    private void comportamientoBotonLogin(){
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Pattern patternMail = Pattern.compile( "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$");
-
-                if(etEmail.getText().toString().equals("")){
-                    etEmail.setError("Campo requerido");
-                }else if(!patternMail.matcher(etEmail.getText().toString()).matches()){
-                    etEmail.setError("Email invalido");
-                }else if(etPassword.getText().toString().equals("")){
-                    etPassword.setError("Campo requerido");
-                }else{
-                    ///////ACA PROGRAMAR LOGIN
+                if (repeatPasswordVisible) {
+                    etRepeatPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    toggleRepeatPasswordButton.setBackgroundResource(R.drawable.eye_close_3); // Icono de ojo cerrado
+                    repeatPasswordVisible = false;
+                } else {
+                    etRepeatPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    toggleRepeatPasswordButton.setBackgroundResource(R.drawable.eye_svgrepo_com); // Icono de ojo abierto
+                    repeatPasswordVisible = true;
                 }
+                etRepeatPassword.setSelection(etRepeatPassword.getText().length()); // Mover el cursor al final del texto
             }
         });
     }
