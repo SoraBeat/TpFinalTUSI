9b86ec3d3a68eb7c6c5c7d3855992e47dfade3c2
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-09-25 16:07:36 -0300

    Registro visual

--- app/src/main/java/com/example/tpfinaltusi/activities/Registro.java
+++ app/src/main/java/com/example/tpfinaltusi/activities/Registro.java
@@ -3,24 +3,39 @@ package com.example.tpfinaltusi.activities;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tpfinaltusi.R;
 
-public class Registro extends AppCompatActivity {
-    boolean passwordVisible = true;
-    boolean repeatPasswordVisible = true;
-    private EditText etPassword;
-    private ImageButton togglePasswordButton;
-    private EditText etRepeatPassword;
-    private ImageButton toggleRepeatPasswordButton;
+import java.util.regex.Pattern;
 
+public class Registro extends AppCompatActivity {
+    private boolean passwordVisible = true;
+    private boolean repeatPasswordVisible = true;
+    private EditText etAlias;
+    private EditText etDni;
+    private EditText etMail;
+    private EditText etPassword;
+    private EditText etRepeatPassword;
+    private ImageButton togglePasswordButton;
+    private ImageButton toggleRepeatPasswordButton;
+    private CheckBox cbCondiciones;
+    private TextView btnCondicionesDelServicio;
+    private Button btnRegistrarse;
+    private TextView btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +57,57 @@ public class Registro extends AppCompatActivity {
 
         setContentView(R.layout.activity_registro);
         /////////////////////////////////////OBTENER ELEMENTOS///////////////////////////////////////////////
+        etAlias = findViewById(R.id.et_alias);
+        etDni = findViewById(R.id.et_dni);
+        etMail = findViewById(R.id.et_mail);
         etPassword = findViewById(R.id.et_password);
-        togglePasswordButton = findViewById(R.id.togglePasswordButton);
         etRepeatPassword = findViewById(R.id.et_repeatPassword);
+        cbCondiciones = findViewById(R.id.cb_TerminosCondiciones);
+        btnCondicionesDelServicio = findViewById(R.id.btn_condicionesDelServicio);
+        togglePasswordButton = findViewById(R.id.togglePasswordButton);
         toggleRepeatPasswordButton = findViewById(R.id.toggleRepeatPasswordButton);
+        btnRegistrarse = findViewById(R.id.btn_registrarse);
+        btnLogin = findViewById(R.id.btn_login);
         /////////////////////////////////////FUNCIONES COMPORTAMIENTO////////////////////////////////////////
         comportamientoMostrarOcultarContrasenia();
         comportamientoMostrarOcultarContraseniaRepetida();
+        comportamientoBotonCondicionesDelServicio();
+        comportamientoBotonLogin();
+        comportamientoBotonRegistro();
+    }
+    private void comportamientoBotonLogin(){
+        //Control boton registrarse
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void comportamientoBotonCondicionesDelServicio() {
+        btnCondicionesDelServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un cuadro de diálogo personalizado para mostrar las condiciones del servicio
+                AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                builder.setTitle("Condiciones del Servicio");
+                builder.setMessage(R.string.terminos_condicones);
+
+                // Botón de Aceptar
+                builder.setPositiveButton(Html.fromHtml("<font color='#47C68F'>Aceptar</font>"), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Cerrar el cuadro de diálogo
+                        dialog.dismiss();
+                    }
+                });
+
+                // Mostrar el cuadro de diálogo
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
     }
     private void comportamientoMostrarOcultarContrasenia(){
         //Controlar mostrar ocultar contraseña
@@ -85,6 +144,41 @@ public class Registro extends AppCompatActivity {
                     repeatPasswordVisible = true;
                 }
                 etRepeatPassword.setSelection(etRepeatPassword.getText().length()); // Mover el cursor al final del texto
+            }
+        });
+    }
+    private void comportamientoBotonRegistro(){
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pattern patternMail = Pattern.compile( "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$");
+                if(etAlias.getText().toString().equals("")){
+                    etAlias.setError("Campo requerido");
+                }else if(etAlias.getText().toString().length()<3){
+                    etAlias.setError("Minimo 3 caracteres");
+                }else if(etAlias.getText().toString().length()>10){
+                    etAlias.setError("Maximo 10 caracteres");
+                }else if(etDni.getText().toString().equals("")){
+                    etDni.setError("Campo requerido");
+                }else if(etDni.getText().toString().length()>10){
+                    etDni.setError("Maximo 10 caracteres");
+                }else if(etMail.getText().toString().equals("")){
+                    etMail.setError("Campo requerido");
+                }else if(!patternMail.matcher(etMail.getText().toString()).matches()){
+                    etMail.setError("Email invalido");
+                }else if(etPassword.getText().toString().equals("")){
+                    etPassword.setError("Campo requerido");
+                }else if(etPassword.getText().toString().length()<3){
+                    etPassword.setError("Minimo 3 caracteres");
+                }else if(etPassword.getText().toString().length()>10){
+                    etPassword.setError("Maximo 10 caracteres");
+                }else if(!etRepeatPassword.getText().toString().equals(etPassword.getText().toString())){
+                    etRepeatPassword.setError("Las contraseñas no coinciden");
+                }else if(!cbCondiciones.isChecked()){
+                    Toast.makeText(getApplicationContext(), "Debe aceptar Terminos y Condiciones", Toast.LENGTH_SHORT).show();
+                }else{
+                    ///////ACA PROGRAMAR REGISTRO
+                }
             }
         });
     }
