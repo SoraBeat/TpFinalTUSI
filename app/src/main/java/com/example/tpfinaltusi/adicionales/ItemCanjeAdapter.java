1f2c4406d17f61c117a43092aec7c15732655211
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-10-15 19:48:33 -0300

    Cambio de controles ya que no funciona Alert dialog por el context :c

--- app/src/main/java/com/example/tpfinaltusi/adicionales/ItemCanjeAdapter.java
+++ app/src/main/java/com/example/tpfinaltusi/adicionales/ItemCanjeAdapter.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +16,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 
 import com.example.tpfinaltusi.Negocio.UsuarioNegocio;
 import com.example.tpfinaltusi.R;
 import com.example.tpfinaltusi.adicionales.activity_canje_exitoso;
 import com.example.tpfinaltusi.entidades.Premio;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -58,24 +61,33 @@ public class ItemCanjeAdapter extends ArrayAdapter<Premio> {
         productPts.setText(String.valueOf(premio.getPrecio()));
         productImag.setImageBitmap(bitmap);
 
+        View finalConvertView = convertView;
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mostrarAlertDialog(context, premio);
+                mostrarAlertDialog(finalConvertView, premio);
             }
         });
 
         return convertView;
     }
 
-    private void mostrarAlertDialog(final Context context, final Premio premio) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AlertDialogCustomStyle);
-        builder.setTitle("Confirmación");
-        builder.setMessage("¿Está seguro de canjear este producto en la posición " + getPosition(premio) + "?");
+    private void mostrarAlertDialog(View view, final Premio premio) {
 
-        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+        Snackbar snackbar = Snackbar.make(view,"", Snackbar.LENGTH_LONG);
+        snackbar.setTextColor(ContextCompat.getColor(context, R.color.black));
+        snackbar.setActionTextColor(ContextCompat.getColor(context, R.color.black));
+        TextView tv2 = snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+        tv2.setVisibility(View.GONE);
+        TextView tv = snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_action);
+        tv.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        tv.setTextSize(20);
+        tv.setAllCaps(false);
+        snackbar.setAction("Apreta aqui para canjear", new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(View view) {
+                // Realiza una acción cuando se hace clic en el botón de acción
                 // Acceder a los valores del premio directamente
                 String title = premio.getNombre();
 
@@ -88,17 +100,9 @@ public class ItemCanjeAdapter extends ArrayAdapter<Premio> {
                 UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
                 usuarioNegocio.restarPuntosAUsuarioPorId(idUsuario, premio.getPrecio());
 
-                context.startActivity(intent);
+                snackbar.getContext().startActivity(intent);
             }
         });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // El usuario ha cancelado el canje, no es necesario realizar ninguna acción aquí.
-            }
-        });
-
-        builder.show();
+        snackbar.show();
     }
 }
