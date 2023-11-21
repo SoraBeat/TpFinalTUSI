a7467412b084383bd5ba8cd2a82283e1b7b96266
LuciaCavana <luciacavana@yahoo.com.ar>
2023-11-21 12:45:48 -0300

    Filtro Canjes V1
    problema con que la lista se queda cargando, si bien muestra unicamente los retirados

--- app/src/main/java/com/example/tpfinaltusi/activities/ActivityListCanje.java
+++ app/src/main/java/com/example/tpfinaltusi/activities/ActivityListCanje.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.tpfinaltusi.Negocio.CanjeNegocio;
@@ -24,6 +27,7 @@ public class ActivityListCanje extends AppCompatActivity {
     ListView listView;
     ProgressBar progressBar;
     ImageView btnBack;
+    Spinner spinnerFiltro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +35,11 @@ public class ActivityListCanje extends AppCompatActivity {
 
         // Inflar el diseño antes de obtener referencias a las vistas
         setContentView(R.layout.activity_list_canje);
+
+        // Obtener referencias a las vistas
+        progressBar = findViewById(R.id.progressBar);
+        listView = findViewById(R.id.lvlistar);
+        spinnerFiltro = findViewById(R.id.spinnerFiltro);
 
         //////////////////////////////////CONFIGURACION ACTION BAR////////////////////////////////////////////
         // Habilitar ActionBar y configurar vista personalizada
@@ -46,7 +55,6 @@ public class ActivityListCanje extends AppCompatActivity {
         btnBack = customActionBarView.findViewById(R.id.btn_back);
         btnBack.setVisibility(View.VISIBLE);
 
-        progressBar = findViewById(R.id.progressBar);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,26 +66,62 @@ public class ActivityListCanje extends AppCompatActivity {
         actionBarTitle.setText("Listar Canje");
 
 
-        listView = findViewById(R.id.lvlistar);
+        //////////////////////////////////CONFIGURACION FILTRO////////////////////////////////////////////
+        // Configurar el ArrayAdapter para el Spinner
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                this, R.array.opciones_filtro, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFiltro.setAdapter(spinnerAdapter);
+        spinnerFiltro.setVisibility(View.VISIBLE);
 
-        CanjeNegocio canjeNegocio = new CanjeNegocio();
-        SharedPreferences sharedPreferences = getSharedPreferences("SesionUsuario", Context.MODE_PRIVATE);
-        canjeNegocio.traerCanjePorIdUsuario(sharedPreferences.getInt("idUsuario", -1), new CanjeNegocio.CanjesCallback() {
+        // Configurar el Listener para el cambio de selección en el Spinner
+        spinnerFiltro.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onCanjesLoaded(List<Canje> canjes) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ListItemCanjeAdapter listItemCanjeAdapter = new ListItemCanjeAdapter(ActivityListCanje.this,ActivityListCanje.this, canjes);
-                        listView.setAdapter(listItemCanjeAdapter);
-                        if (progressBar != null) {
-                            progressBar.setVisibility(View.VISIBLE); // o View.GONE según tus necesidades
-                        }
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                // Obtener la opción seleccionada en el Spinner
+                String opcion = adapterView.getItemAtPosition(position).toString();
 
+                // Convertir la opción a un valor numérico (1, 2, o 3)
+                int valorNumerico = 1;
+                if (opcion.equals("Todo")) {
+                    valorNumerico = 1;
+                } else if (opcion.equals("Pendiente")) {
+                    valorNumerico = 2;
+                } else if (opcion.equals("Retirado")) {
+                    valorNumerico = 3;
+                }
+
+                //////////////////////////////////CONFIGURACION NORMAL VISTA////////////////////////////////////////////
+                CanjeNegocio canjeNegocio = new CanjeNegocio();
+                SharedPreferences sharedPreferences = getSharedPreferences("SesionUsuario", Context.MODE_PRIVATE);
+                int finalValorNumerico = valorNumerico;
+                canjeNegocio.traerCanjePorIdUsuario(sharedPreferences.getInt("idUsuario", -1), new CanjeNegocio.CanjesCallback() {
+                    @Override
+                    public void onCanjesLoaded(List<Canje> canjes) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ListItemCanjeAdapter listItemCanjeAdapter = new ListItemCanjeAdapter(ActivityListCanje.this,ActivityListCanje.this, canjes, finalValorNumerico);
+                                listView.setAdapter(listItemCanjeAdapter);
+                                if (progressBar != null) {
+                                    progressBar.setVisibility(View.VISIBLE); // o View.GONE según tus necesidades
+                                }
+
+                            }
+                        });
                     }
                 });
+                // Hacer algo con el valor numérico, por ejemplo, filtrar la lista
+                // utilizando el valor en tu adaptador (no proporcionado en este código).
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // No se necesita implementación aquí
             }
         });
+
+
 
 
     }
--- app/src/main/java/com/example/tpfinaltusi/adicionales/ListItemCanjeAdapter.java
+++ app/src/main/java/com/example/tpfinaltusi/adicionales/ListItemCanjeAdapter.java
@@ -36,19 +36,40 @@ import java.util.List;
 public class ListItemCanjeAdapter extends ArrayAdapter<Canje> {
     private Context context;
     private ActivityListCanje activity;
+    private int filtro;
 
-    public ListItemCanjeAdapter(ActivityListCanje activity, Context context, List<Canje> canjes) {
+    public ListItemCanjeAdapter(ActivityListCanje activity, Context context, List<Canje> canjes, int filtro) {
         super(context, 0, canjes);
         this.context = context;
         this.activity = activity;
+        this.filtro = filtro;
     }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
+
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.adapter_list_item_canjes, parent, false);
         }
+
+        if(filtro == 1){
+            Llenar_view(position, convertView, parent);
+        }
+        else if(filtro == 2){
+            Llenar_view_filter(position, convertView, parent,false);
+
+        }
+        else if(filtro == 3){
+            Llenar_view_filter(position, convertView, parent,true);
+        }
+
+
+        return convertView;
+    }
+
+    void Llenar_view(final int position, View convertView, ViewGroup parent){
+
         // Obtener una instancia del Producto en la posición actual
         Canje canje = getItem(position);
 
@@ -64,8 +85,6 @@ public class ListItemCanjeAdapter extends ArrayAdapter<Canje> {
         progressBar.setVisibility(View.VISIBLE);
 
         canjeNumero.setText(String.valueOf(canje.getIdCanje()));
-
-
         if(canje.isEstado()){
             canjeEstado.setText("Retirado");
         }
@@ -164,8 +183,125 @@ public class ListItemCanjeAdapter extends ArrayAdapter<Canje> {
                 });
             }
         });
+    }
 
-        return convertView;
+    void Llenar_view_filter(final int position, View convertView, ViewGroup parent, boolean estado){
+        // Obtener una instancia del Producto en la posición actual
+        Canje canje = getItem(position);
+
+        // Obtener referencias a las vistas dentro del elemento personalizado
+        TextView productTitle = convertView.findViewById(R.id.product_title);
+        TextView canjeEstado = convertView.findViewById(R.id.Estado);
+        TextView canjeNumero = convertView.findViewById(R.id.txt_Numero_canje);
+        ImageView productImag = convertView.findViewById(R.id.product_image);
+        TextView puntoverdeDireccion = convertView.findViewById(R.id.Direccion);
+        ProgressBar progressBar = convertView.findViewById(R.id.cargando);
+        LinearLayout contenedor = convertView.findViewById(R.id.contenedor);
+        contenedor.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        if(estado == canje.isEstado()){
+
+            canjeNumero.setText(String.valueOf(canje.getIdCanje()));
+            if(canje.isEstado()){
+                canjeEstado.setText("Retirado");
+            }
+            else{
+                canjeEstado.setText("Pediente");
+            }
+
+
+            PremioNegocio premioNegocio = new PremioNegocio();
+            premioNegocio.traerPremioPorId(canje.getIdPremio(), new PremioNegocio.PremioCallback() {
+                @Override
+                public void onSuccess(String mensaje) {
+
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+
+                @Override
+                public void onPremioLoaded(Premio premio) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Seteamos bitmap para la imagen
+                            String pureBase64Encoded = premio.getImagen().substring(premio.getImagen().indexOf(",") + 1);
+                            byte[] imageBytes = Base64.decode(pureBase64Encoded, Base64.DEFAULT);
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+                            // Asignar los datos del Producto a las vistas
+                            productTitle.setText(premio.getNombre());
+                            productImag.setImageBitmap(bitmap);
+                        }
+                    });
+
+                }
+            });
+
+            PuntoVerdeNegocio puntoVerdeNegocio = new PuntoVerdeNegocio();
+            puntoVerdeNegocio.traerPuntoVerdePorId(canje.getIdPuntoVerde(), new PuntoVerdeNegocio.PuntoVerdeCallback() {
+                @Override
+                public void onSuccess(String mensaje) {
+
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+
+                @Override
+                public void onPuntoVerdeLoaded(PuntoVerde puntoVerde) {
+                    LocalidadNegocio localidadNegocio = new LocalidadNegocio();
+                    localidadNegocio.traerLocalidadPorId(puntoVerde.getIdLocalidad(), new LocalidadNegocio.LocalidadCallback() {
+                        @Override
+                        public void onSuccess(String mensaje) {
+
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+
+                        @Override
+                        public void onLocalidadLoaded(Localidad localidad) {
+                            ProvinciaNegocio provinciaNegocio = new ProvinciaNegocio();
+                            provinciaNegocio.traerProvinciaPorId(localidad.getIdProvincia(), new ProvinciaNegocio.ProvinciaCallback() {
+                                @Override
+                                public void onSuccess(String mensaje) {
+
+                                }
+
+                                @Override
+                                public void onError(String error) {
+
+                                }
+
+                                @Override
+                                public void onProvinciaLoaded(Provincia provincia) {
+                                    activity.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            contenedor.setVisibility(View.VISIBLE);
+                                            progressBar.setVisibility(View.GONE);
+                                            puntoverdeDireccion.setText( provincia.getNombre()+", "+localidad.getNombre()+ ", "+ puntoVerde.getCalleAltura());
+                                        }
+                                    });
+
+                                }
+                            });
+
+
+                        }
+                    });
+                }
+            });
+        }
     }
 
 }
